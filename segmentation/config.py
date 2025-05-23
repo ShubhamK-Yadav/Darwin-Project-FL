01298368be019c7e7d1d7149f9ed037d69cea9de
Shubham K Yadav <47945015+ShubhamK-Yadav@users.noreply.github.com>
2025-05-23 03:23:41 +0100

    Update config.py
    Path updated

--- segmentation/config.py
+++ segmentation/config.py
@@ -10,48 +10,44 @@ PHASE = 'detect'   # 'seg' or 'detect'
 NUM_CLASSES = 2 if 'seg' in PHASE else 3
 
 DEVICE = '0'
-# True if use internal pre-trained model
-# Must be True when pre-training and inference
-PRE_TRAINED = False
-# True if use resume model
-CKPT_POINT = False
+PRE_TRAINED = False  # Must be True when pre-training and inference
+CKPT_POINT = False   # True if use resume model
 
 FOLD_NUM = 5
-# [1-FOLD_NUM]
 CURRENT_FOLD = 5
 GPU_NUM = len(DEVICE.split(','))
 
 #--------------------------------- mode and data path setting
-PATH_DIR = '/mnt/parscratch/users/aca21sky/preprocessed_output/segmentation/detectdata/data_2d'
-PATH_LIST = glob.glob(os.path.join(PATH_DIR,'*.hdf5'))
-PATH_AP = '/mnt/parscratch/users/aca21sky/preprocessed_output/segmentation/detectdata/data_3d'
-AP_LIST = glob.glob(os.path.join(PATH_AP,'*.hdf5'))
+PATH_DIR = '/path/to/preprocessed_output/segmentation/detectdata/data_2d'
+PATH_LIST = glob.glob(os.path.join(PATH_DIR, '*.hdf5'))
+PATH_AP = '/path/to/preprocessed_output/segmentation/detectdata/data_3d'
+AP_LIST = glob.glob(os.path.join(PATH_AP, '*.hdf5'))
 #---------------------------------
 
-CKPT_PATH = './new_ckpt/{}/{}/fold{}'.format(PHASE,VERSION,str(CURRENT_FOLD))
+CKPT_PATH = './new_ckpt/{}/{}/fold{}'.format(PHASE, VERSION, str(CURRENT_FOLD))
 WEIGHT_PATH = get_weight_path(CKPT_PATH)
 print(WEIGHT_PATH)
 
-#you could set it for your device
+# You could set it for your device
 INIT_TRAINER = {
-  'num_classes':NUM_CLASSES,
-  'n_epoch':160,
-  'batch_size':24,
-  'num_workers':4,
-  'device':DEVICE,
-  'pre_trained':PRE_TRAINED,
-  'ckpt_point':CKPT_POINT,
-  'weight_path':WEIGHT_PATH,
-  'use_fp16':False,
-  'transformer_depth': TRANSFORMER_DEPTH,
-  'use_transfer_learning': True,
-  'pretrained_backbone': 'resnet34',
-  'lr': 1e-4  # Lower learning rate for stability
- }
+    'num_classes': NUM_CLASSES,
+    'n_epoch': 160,
+    'batch_size': 24,
+    'num_workers': 4,
+    'device': DEVICE,
+    'pre_trained': PRE_TRAINED,
+    'ckpt_point': CKPT_POINT,
+    'weight_path': WEIGHT_PATH,
+    'use_fp16': False,
+    'transformer_depth': TRANSFORMER_DEPTH,
+    'use_transfer_learning': True,
+    'pretrained_backbone': 'resnet34',
+    'lr': 1e-4  # Lower learning rate for stability
+}
 #---------------------------------
 
 SETUP_TRAINER = {
-  'output_dir':'./new_ckpt/{}/{}'.format(PHASE,VERSION),
-  'log_dir':'./new_log/{}/{}'.format(PHASE,VERSION),
-  'phase':PHASE,
-  }
+    'output_dir': './new_ckpt/{}/{}'.format(PHASE, VERSION),
+    'log_dir': './new_log/{}/{}'.format(PHASE, VERSION),
+    'phase': PHASE,
+}
